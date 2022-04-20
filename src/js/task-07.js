1902865b28773787fc02cc28c70b48a96273daf1
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-04-20 22:11:15 +0300

    Решает задачи 7-8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,3 +2,20 @@
 // (событие input) и изменяет инлайн - стиль span#text обновляя свойство font-size.
 // В результате при перетаскивании ползунка будет меняться размер текста.
 
+// ========== 1 завдання ============
+
+// Отримуємо доступ до елементів до input та span
+const inputEl = document.querySelector('#font-size-control');
+const spanEl = document.querySelector('#text');
+
+// Ставимо слухача подій на input
+inputEl.addEventListener('input', onRangeMove);
+
+// Функція одержує поточне значення input та
+function onRangeMove(event) {
+   const fontSizeSpan = event.currentTarget.value;
+
+   // переписує значення властивості fontSize об'єкта стилів span
+   spanEl.style.fontSize = `${fontSizeSpan}px`;
+  
+}
