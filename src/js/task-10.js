6c911d1a82f869757af4b6cb7ffef9d3117d0345
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-04-24 21:55:12 +0300

    Закінчує дз

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -26,34 +26,9 @@ const btnCreateEl = document.querySelector('button[data-create]');
 const btnDestroyEl = document.querySelector('button[data-destroy]');
 const divEl = document.querySelector('div#boxes');
 
-// btnCreateEl.addEventListener('click', onCreateBtnClick);
-
-// function onCreateBtnClick() {
-//   const boxQuantity = Number(inputEl.value);
-  
-//   for (let i = 0; i < boxQuantity; i += 1) {
-    
-//     const divBox = document.createElement('div');
-    
-//     divBox.style.width = `${i * 10 + 30}px`;
-//     divBox.style.height = `${i * 10 + 30}px`;
-//     divBox.style.backgroundColor = getRandomHexColor();
-    
-//     divEl.append(divBox);
-//   }
-// }
-
-// function createBoxes(amount) {
-  
-// }
-
-
-
-// Далі функція очищує значення елементів форми
-        // event.currentTarget.reset();
-
-// ================================================
-
+// Ставимо слухача на кнопки по події клік
+btnCreateEl.addEventListener('click',  createBoxes);
+btnDestroyEl.addEventListener('click', destroyBoxes);
 
 // Функція сторення div
 function createBoxes() {
